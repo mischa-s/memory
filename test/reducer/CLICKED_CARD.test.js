c268cc477675dd56b61e16ecce5858fddad22177
bill-burgess <wmhburgess@gmail.com>
2017-01-19 20:04:25 +1300

    adds CLICKED_CARD test

--- test/reducer/CLICKED_CARD.test.js
+++ test/reducer/CLICKED_CARD.test.js
@@ -2,15 +2,143 @@ const test = require('tape')
 const freeze = require('deep-freeze')
 const reducer = require('../../src/reducer')
 
-test('', t => {
+test('CLICKED_CARD | should check if the card matches the cardRevealed or reveal it if no card is revealed', t => {
 
   const state = {
-    cards: {},
+    cards: {
+      1: {
+        id: 3,
+        value: 'blue',
+        visable: false
+      },
+      2: {
+        id: 1,
+        value: 'red',
+        visable: false
+      },
+      3: {
+        id: 4,
+        value: 'blue',
+        visable: false
+      },
+      4: {
+        id: 2,
+        value: 'red',
+        visable: false
+      }
+    },
     cardRevealed: null,
     highScores: {}
   }
   freeze(state)
 
+  const expectedState1 = {
+    cards: {
+      1: {
+        id: 3,
+        value: 'blue',
+        visable: false
+      },
+      2: {
+        id: 1,
+        value: 'red',
+        visable: false
+      },
+      3: {
+        id: 4,
+        value: 'blue',
+        visable: false
+      },
+      4: {
+        id: 2,
+        value: 'red',
+        visable: false
+      }
+    },
+    cardRevealed: 3,
+    highScores: {}
+  }
+
+  const action1 = {
+    type: 'CLICKED_CARD',
+    payload: 3
+  }
+
+  const newState1 = reducer(state, action1)
+
+  t.deepEqual(expectedState1, newState1, 'should change cardRevealed to the clicked card if it was null')
+
+  const expectedState2 = {
+    cards: {
+      1: {
+        id: 3,
+        value: 'blue',
+        visable: false
+      },
+      2: {
+        id: 1,
+        value: 'red',
+        visable: false
+      },
+      3: {
+        id: 4,
+        value: 'blue',
+        visable: false
+      },
+      4: {
+        id: 2,
+        value: 'red',
+        visable: false
+      }
+    },
+    cardRevealed: null,
+    highScores: {}
+  }
+
+  const action2 = {
+    type: 'CLICKED_CARD',
+    payload: 2
+  }
+
+  const newState2 = reducer(newState1, action2)
+
+  t.deepEqual(expectedState2, newState2, 'should change revealed card back to null if no match')
+
+  const expectedState3 = {
+    cards: {
+      1: {
+        id: 3,
+        value: 'blue',
+        visable: true
+      },
+      2: {
+        id: 1,
+        value: 'red',
+        visable: false
+      },
+      3: {
+        id: 4,
+        value: 'blue',
+        visable: true
+      },
+      4: {
+        id: 2,
+        value: 'red',
+        visable: false
+      }
+    },
+    cardRevealed: null,
+    highScores: {}
+  }
+
+  const action3 = {
+    type: 'CLICKED_CARD',
+    payload: 1
+  }
+
+  const newState3 = reducer(newState1, action3)
+
+  t.deepEqual(expectedState3, newState3, 'should change cards to visable if a match')
 
   t.end()
 })
--- test/reducer/RANDOMISE_CARDS.test.js
+++ test/reducer/RANDOMISE_CARDS.test.js
@@ -18,13 +18,13 @@ test('RANDOMISE_CARDS | adds two sets of the cards, randomly ordered, into the s
   const newState = reducer(state, action)
 
   const cardKeys = Object.keys(newState.cards)
-  function cardHasId4(key){
-    return newState.cards[key].id === 4
+  function cardHasValue4(key){
+    return newState.cards[key].value === 4
   }
   const expectedKeysOfCard = ['id', 'value', 'visable']
 
   t.is(cardKeys.length, 16, 'there should be exactly 16 cards')
-  t.is(cardKeys.filter(cardHasId4).length, 2, 'there should be 2 cards with id 4')
+  t.is(cardKeys.filter(cardHasValue4).length, 2, 'there should be 2 cards with id 4')
   t.deepEqual(Object.keys(newState.cards[7]), expectedKeysOfCard, 'the cards should have the appriate keys')
 
   t.end()
