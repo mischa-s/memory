d4300be0f1668611da320afb672c86e09529a3bd
bill-burgess <wmhburgess@gmail.com>
2017-01-20 10:19:27 +1300

    adds reducer method RANDOMISE_CARDS

--- src/reducer.js
+++ src/reducer.js
@@ -4,7 +4,23 @@ const _ = require('lodash')
 module.exports = function (state, action) {
   const newState = clone(state)
   switch (action.type) {
+
     case 'RANDOMISE_CARDS':
+      const oneSetCardValues = ['Red', 'Blue', 'Yellow', 'Green', 'Orange', 'Purple', 'Black', 'White']
+      const cardValues = []
+      oneSetCardValues.forEach(cardValue => {
+        cardValues.push(cardValue)
+        cardValues.push(cardValue)
+      })
+      for (var i = 1; i < 17; i++) {
+        const randomCardPosition = Math.floor(Math.random() * cardValues.length)
+        const card = {
+          id: i,
+          value: cardValues.splice(randomCardPosition, 1),
+          visable: false
+        }
+        newState.cards[i] = (card)
+      }
       return newState
   }
 }
