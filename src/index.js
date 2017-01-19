6b4c32bb590f42091da510b068749fcf9d5d76be
rodrigo-delgado <rod.delgado85@gmail.com>
2017-01-19 18:46:04 +1300

    static input and button showing up

--- src/index.js
+++ src/index.js
@@ -3,82 +3,28 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const { createStore } = require('redux')
 const reducer = require('./reducer')
+//Below is es6 object destructuring
+const {Route, Router, IndexRoute, hashHistory} = require('react-router')
+//Components
+const SubmitName = require('../src/components/SubmitName')
 
-// components
-const App = require('./components/app')
-const CounterApp = require('./components/counter-app')
-
-
-
-// actions
-// plain object {type: string, payload: Object | string | number}
-
-// model -> state
-
-// reducer (state, action) :: -> state  
-//const initialState = 0
-const initialState = {
-  products: {
-    1: {id: 1, name: 'banana', stock: 2, price:2} 
-  },
-
-  cart:  {
-
-  },
-
-  total: 0
-}
-
-
-const store = createStore(reducer, initialState)
-// store .dispatch(action)
-// reducer -> state
-// store.subscribe
-
-// store .getState -> state
-console.log('store', store)
-console.log('state', store.getState())
+const store = createStore(reducer)
 
 document.addEventListener('DOMContentLoaded', (e) => {
 
   store.subscribe(() => {
-    const state = store.getState()
-    console.log('state', state)
-    render(state)
-  })
+     const state = store.getState()
+     render(state)
+   })
 
+   function render (state) {
+       const root = document.querySelector('#app')
 
-  function render (state) {
-    const root = document.querySelector('#app')
-    ReactDOM.render(
-      <App state={state} store={store} />,
-      root
-    )
-  }
-
-  render(store.getState())
-
-
-
-
-
-  //store.fetchCats()
-
+       ReactDOM.render(
+         <SubmitName />,
+         root
+       )
+     }
+     store.dispatch({type: 'GO!'})
 
 })
-
-
-
-
-
-function counterReducer (state, action) {
-  console.log('reducer', state, action)
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    default: // must have default
-      return state
-  }
-}
